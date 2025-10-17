5c94819f51c2acefeb55918d6961b13f2a21a998
Girish-Kanakuntla <girishkanakuntla@eng.rizvi.edu.in>
2025-10-17 13:41:12 +0530

    Add files via upload
    Upload 1.1 Exp

---
+++ Experiment-1.1.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+
+int main(){
+    /* Girish Gangadhar Kanakuntla
+    18
+    First Year
+    D
+    251P113
+    */
+
+printf("Name: Girish Gangadhar Kanakuntla\n");
+printf("Age: 18\n");
+printf("Class: First Year\n");
+printf("Division: D\n");
+printf("UIN: 251P113\n");
+
+return 0;
+}
