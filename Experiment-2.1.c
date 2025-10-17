75907c88b95f471a55c6abbb5feea45d7166f610
Girish-Kanakuntla <girishkanakuntla@eng.rizvi.edu.in>
2025-10-17 13:44:02 +0530

    Add files via upload
    Exp 2.1

---
+++ Experiment-2.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ /*Girish Kanakuntla First Year- D - Computer
+Uin- 251P113*/
+
+int main() {
+int num;
+printf("Enter a Number:");
+scanf("%d",&num);
+
+if(num%2 == 0){
+    printf("Even Number");
+}
+else{
+    printf("Odd Number");
+}
+    return 0;
+}
