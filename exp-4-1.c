3079ddd002beab5d970a7588d9b81325ea05d234
Girish-Kanakuntla <girishkanakuntla@eng.rizvi.edu.in>
2025-12-04 07:36:01 -0800

    Optimize isPrime function for better performance
    Updated the prime-checking logic to improve efficiency.

--- exp-4-1.c
+++ exp-4-1.c
@@ -6,7 +6,7 @@ int isPrime(int num) {
     if (num <= 1)
         return 0; 
 
-    for (i = 2; i <= num / 2; i++) {
+    for (i = 2; i*i <= num / 2; i++) {
         if (num % i == 0)
             return 0; 
     }
@@ -35,4 +35,5 @@ int main() {
     printPrimes(start, end);
 
     return 0;
+
 }
